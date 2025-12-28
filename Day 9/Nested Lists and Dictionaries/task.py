5d692db85e0ceee51ff91d2159951b55d48c124f
devmit82 <uemit.cicek@gmx.de>
2025-12-28 23:53:57 +0100

    Day 9 bearbeitet bis: Task 2 Pause 2

---
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -0,0 +1,10 @@
+*travel_log = {
+    "Frankreich" : ["Paris", "Lille", "Dijon"],
+    "Deutschland" : ["Stuttgart", "Berlin"]
+}
+
+#print(travel_log["Frankreich"][1])
+
+nested_list = ["A", "B", ["C", "D"]]
+
+print(nested_list[2][1])
