43d2bc0106ba9e8a698ba36ad9e789dce8c68819
devmit82 <uemit.cicek@gmx.de>
2025-12-29 23:13:06 +0100

    Verschachtelte Listen und Wörter beendet. Mit Blind-Auktion-Projekt gestartet. Logo lädt, Todo 1 erledigt.

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,34 @@
+# show logo
+from art import logo
+print(logo)
+
+def get_bids():
+    bid = {}
 # TODO-1: Ask the user for input
+    name = input("What is your name?: ")
+#price als int damit wir später die zahlen vergleich können
+    price = int(input("Whatis your bid?: "))
 # TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
+    # HIER HEUTE WEITER MACHEN, DICTIONARY RICHTIG ANLEGEN!!
+    bid[name] = price
+    # TODO-3: Whether if new bids need to be added
+    # ask_next = True
+    # while ask_next == True:
+    #     next_bid = input("Are there any other bidders? Type: 'yes' or 'no'")
+    #     if next_bid == "yes":
+    #         print("\n" * 100)
+    #         bid[name] = input("What is your name?: ")
+    #          = int(input("Whatis your bid?: "))
+    #         print(bid)
+        # else:
+        #     ask_next = False
+        #     highest_bid = max(bid)
+        #     print(max(bid.()))
+
+get_bids()
+
+
+
 # TODO-4: Compare bids in dictionary
 
 
--- Day 9/Nested Lists and Dictionaries/task.py
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -1,10 +1,23 @@
-*travel_log = {
-    "Frankreich" : ["Paris", "Lille", "Dijon"],
-    "Deutschland" : ["Stuttgart", "Berlin"]
-}
+# travel_log = {
+#     "Frankreich" : ["Paris", "Lille", "Dijon"],
+#     "Deutschland" : ["Stuttgart", "Berlin"]
+#      }
 
 #print(travel_log["Frankreich"][1])
 
-nested_list = ["A", "B", ["C", "D"]]
+#nested_list = ["A", "B", ["C", "D"]]
 
-print(nested_list[2][1])
+#print(nested_list[2][1])
+
+travel_log = {
+    "Frankreich" : {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    "Deutschland" : {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart" ],
+        "total_visits": 5
+    }
+}
+
+print(travel_log["Deutschland"]["cities_visited"][2])
