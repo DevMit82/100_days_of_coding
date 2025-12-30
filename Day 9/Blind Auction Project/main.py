586d16a4b1a4297f11ac2731d48573ef2dbe5300
devmit82 <uemit.cicek@gmx.de>
2025-12-30 21:12:30 +0100

    Day 9 beendet

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,34 +1,37 @@
+import os
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
 # show logo
 from art import logo
 print(logo)
 
 def get_bids():
-    bid = {}
-# TODO-1: Ask the user for input
-    name = input("What is your name?: ")
-#price als int damit wir später die zahlen vergleich können
-    price = int(input("Whatis your bid?: "))
-# TODO-2: Save data into dictionary {name: price}
-    # HIER HEUTE WEITER MACHEN, DICTIONARY RICHTIG ANLEGEN!!
-    bid[name] = price
-    # TODO-3: Whether if new bids need to be added
-    # ask_next = True
-    # while ask_next == True:
-    #     next_bid = input("Are there any other bidders? Type: 'yes' or 'no'")
-    #     if next_bid == "yes":
-    #         print("\n" * 100)
-    #         bid[name] = input("What is your name?: ")
-    #          = int(input("Whatis your bid?: "))
-    #         print(bid)
-        # else:
-        #     ask_next = False
-        #     highest_bid = max(bid)
-        #     print(max(bid.()))
+    bids = {}
+    more_bidder = True
+    while more_bidder is True:
+    # TODO-1: Ask the user for input
+        name = input("What is your name?: ")
+        price = int(input("What is your bid?: $"))
+        # TODO-2: Save data into dictionary {name: price}
+        bids[name] = price
+            # TODO-3: Whether if new bids need to be added
+        new_bids = input("Are there any other bidders? Type 'yes 'or 'no'").lower()
+        if new_bids == "yes":
+            clear_screen()
+        else:
+            more_bidder = False
+            # TODO-4: Compare bids in dictionary
+            # max()läuft über die Keys
+            # key = bids.get sagt: „Vergleiche die Values“
+            # Ergebnis ist der Name des Gewinners
+            winner = max(bids, key=bids.get)
+            # Value von winner abrufen
+            highest_bid = bids[winner]
+            print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 get_bids()
 
 
 
-# TODO-4: Compare bids in dictionary
 
 
