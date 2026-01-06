fa8980f1edcbbdd3b35bc14b44773ede5d63c80b
devmit82 <uemit.cicek@gmx.de>
2026-01-06 22:09:47 +0100

    Until hint 11 Done.

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -5,14 +5,38 @@ cards = [
 ]
 
 def deal_card():
-    new_card = random.randint(-4, 0)
+    new_card = random.randint(0, 12)
     return cards[new_card]
 
 def calculate_score(cards_list):
     score = sum(cards_list)
     if sum(cards_list) == 21:
         return 0
+
+    elif 11 in cards_list and score > 21:
+        cards_list.remove(11)
+        cards_list.append(1)
+        score = sum(cards_list)
+
     return score
+
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "It is a draw."
+    elif computer_score == 0:
+        return "Opponent has a Blackjack! You lose. :-("
+    elif user_score == 0:
+        return "You have a Blackjack! You win :-)"
+    elif user_score > 21:
+        return "You went over. You lose :-("
+    elif computer_score > 21:
+        return "Opponent went over. You win :-)"
+    else:
+        if computer_score > user_score:
+            return "You lose :-("
+        else:
+            return "You win :-)"
+
 
 
 from art import logo
@@ -21,16 +45,48 @@ start_game = input("Do you want to play a game of Blackjack? Type 'y' or 'no': "
 if start_game == "y":
 # Print Logo
     print(logo)
+
 user_cards = []
 computer_cards = []
+game_over = False
 
 user_cards.append(deal_card())
 user_cards.append(deal_card())
+
+computer_cards.append(deal_card())
+computer_cards.append(deal_card())
+
 user_score = calculate_score(user_cards)
-computer_cards.append(deal_card())
-computer_cards.append(deal_card())
+computer_score = calculate_score(computer_cards)
+
 print(f"Your cards: {user_cards}, current score: {user_score}" )
-print(computer_cards[0])
+print(f"Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21 or computer_score > 21:
+    game_over = True
+
+while not game_over:
+    another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+
+    if another_card == "y":
+        user_cards.append(deal_card())
+        user_score = calculate_score(user_cards)
+        print(f"Your cards: {user_cards}, current score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
+        if user_score == 0 or user_score > 21:
+            game_over = True
+
+    else:
+        game_over = True
+
+while computer_score < 17 and user_score != 0 and user_score < 21:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
+
+
+print(f"Your final hand {user_cards}, final score: {user_score}")
+print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+print(compare(user_score, computer_score))
 
 
 # print cards of the user and current score
