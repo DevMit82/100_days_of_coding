120f6aca997cdacacff68b0c2e5b8c5712041f4c
Devmit82 <uemit.cicek@gmx.de>
2026-01-26 21:02:07 +0100

    Add comments

--- Day 16/main.py
+++ Day 16/main.py
@@ -2,23 +2,29 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
+# Create instances of each class
 my_menu = Menu()
 my_coffee_maker = CoffeeMaker()
 my_money_machine = MoneyMachine()
 
-
+# Flag to control whether the machine is runnin
 is_on = True
-
+# Main program loop
 while is_on:
+    # Ask the user for their choice and show available menu items
     user_select = input(f"What would you like? ({my_menu.get_items()}) ")
+    # Turn off the machine
     if user_select == "off":
         is_on = False
+    # Print a report of current resources
     elif user_select == "report":
         my_coffee_maker.report()
     else:
+        # Handle drink selectio
         drink = my_menu.find_drink(user_select)
+        # Check if resources are sufficient AND payment is successful
         if my_coffee_maker.is_resource_sufficient(drink) and my_money_machine.make_payment(drink.cost):
+            # If both checks pass, make the coffee
             my_coffee_maker.make_coffee(drink)
 
 
