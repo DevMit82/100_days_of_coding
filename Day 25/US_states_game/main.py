63ee2f839dd9f987e1ae7cc9cda7d1075ae503be
Devmit82 <uemit.cicek@gmx.de>
2026-02-26 17:35:16 +0100

    Finish Day 25

--- Day 25/US_states_game/main.py
+++ Day 25/US_states_game/main.py
@@ -1,16 +1,42 @@
-import turtle
+from turtle import Turtle, Screen
+import pandas
 
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
 
+writing_t = Turtle()
+writing_t.ht()
+picture_t = Turtle()
 
 image = "blank_states_img.gif"
-screen = turtle.Screen()
+screen = Screen()
 screen.title("U.S. States Game")
 screen.addshape(image)
-turtle.shape(image)
+picture_t.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+data = pandas.read_csv("50_states.csv")
+state_list = data.state.to_list()
 
-# turtle.onscreenclick(get_mouse_click_coor)
-# turtle.mainloop()
+STATES = 50
+score = 0
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+
+while score < STATES:
+    if answer_state == "Exit":
+        break
+    elif answer_state in state_list:
+        row = data[data.state == answer_state]
+        x_cor = int(row.x.iloc[0])
+        y_cor = int(row.y.iloc[0])
+        writing_t.teleport(x= x_cor, y= y_cor)
+        writing_t.write(answer_state, align="center", font=("Arial", 8, "bold"))
+        score += 1
+        state_list.remove(answer_state)
+        answer_state = screen.textinput(title=f"{score}/{STATES} States Correct",
+                                        prompt="What's another state's name?").title()
+    else:
+        answer_state = screen.textinput(title=f"{score}/{STATES} States Correct",
+                                        prompt="What's another state's name?").title()
+
+data = pandas.DataFrame(state_list)
+data.to_csv("states_to_learn.csv")
