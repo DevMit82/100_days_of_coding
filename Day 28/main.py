07b7354b0667b1a7ad143a5dbf6271cabce14cfb
Devmit82 <uemit.cicek@gmx.de>
2026-03-13 23:37:26 +0100

    Add all functions and complete Day 28

--- Day 28/main.py
+++ Day 28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,22 +9,69 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+check_mark = "✓"
+timer = None
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    lab_timer.config(text="Timer")
+    canvas.itemconfig(timer_text, text="00:00")
+    lab_check_mark.config(text="")
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN  * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    if reps % 8 == 0:
+        lab_timer.config(text="Long Break", fg=RED)
+        count_down(long_break_sec)
+
+    elif reps % 2 == 0:
+        lab_timer.config(text="Short Break", fg=PINK)
+        count_down(short_break_sec)
+
+    else:
+        lab_timer.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    global timer
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            mark += check_mark
+        lab_check_mark.config(text=f"{mark}")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_png = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_png)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 ####################################################################
 ###Labels:
@@ -31,16 +79,18 @@ canvas.grid(column=1, row=1)
 lab_timer = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), bg=YELLOW)
 lab_timer.grid(column=1, row=0)
 
-lab_check_mark = Label(text="✓", font=(FONT_NAME, 10, "bold"), fg=GREEN, bg=YELLOW)
+lab_check_mark = Label(font=(FONT_NAME, 10, "bold"), fg=GREEN, bg=YELLOW)
 lab_check_mark.grid(column=1, row=3)
 ####################################################################
 ###Buttons:
 
-btn_start = Button(text="Start",font=(FONT_NAME, 10, "bold",),highlightthickness=0)
+btn_start = Button(text="Start",font=(FONT_NAME, 10, "bold",),highlightthickness=0, command=start_timer)
 btn_start.grid(column=0, row=2)
 
-btn_reset = Button(text="Reset",font=(FONT_NAME, 10, "bold",),highlightthickness=0)
+btn_reset = Button(text="Reset",font=(FONT_NAME, 10, "bold",),highlightthickness=0, command=reset_timer)
 btn_reset.grid(column=2, row=2)
+#####################################################################################
+
 
 
 window.mainloop()
